e828c1fcc7cbc9b2585751833c1f3f3573ee301f
sarikadhene <sarikadhene1105@gmail.com>
2024-08-24 00:04:47 +0530

    automation tesAfter uploading project from

---
+++ src/ex_22082024/Lab0015 reverse.py
@@ -0,0 +1,14 @@
+# for variable_name (start, stop-1, step)
+print("enter the number : ")
+for i in range(10, 0, -1):
+    print(i, end=" ")
+
+print("\n\nenter the number  for reverse print: ")
+for j in range(10, 0, -2):
+    print(j, end=" ")
+
+
+print("\n\nUsing while loop: ")
+while i < 10:
+    print(i, end=" ")
+    i = i + 1
---
+++ src/ex_22082024/lab0017 pass.py
@@ -0,0 +1,20 @@
+for i in range(1,10) :
+    if i % 2 == 0:
+        print(i, end=" ")
+    else:
+        pass
+
+print("\n")
+for i in range(100,1,-1) :
+    if i%3== 0:
+        print(i,end=" ")
+    else:
+        pass
+
+print("\n")
+for i in range(200,1,-1) :
+    if i%5== 0 or i%2==0:
+        print(i,end=" ")
+    else:
+        pass
+
